90f33a5e6b4b872dce95115d154d6740d9811753
Ivan Vizcaino <ivanandres1305@gmail.com>
2026-07-03 15:25:26 -0500

    Initialize `saim-clinical-module-service` project with Clean Architecture implementation, hexagonal structure, Gradle configuration, Docker setup, and essential Spring Boot settings.

---
+++ applications/app-service/src/main/java/co/clinicadelacosta/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.clinicadelacosta.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.clinicadelacosta.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
